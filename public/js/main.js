8d36c1fd4aab5a1014ccae8d54b091dbe6bd2cf0
Alioune <sarr43@gmail.com>
2024-01-24 20:23:07 +0000

    Accueil et Galerie

--- public/js/main.js
+++ public/js/main.js
@@ -159,6 +159,13 @@ document.addEventListener("DOMContentLoaded", function () {
         document.location.href = url;
     });
 
+    $('#gallery').photobox('a', {thumbs: true}, callback);
+    // using setTimeout to make sure all images were in the DOM, before the history.load() function is looking them up to match the url hash
+    setTimeout(window._photobox.history.load, 1000);
+
+    function callback() {
+        console.log('callback for loaded content:', this);
+    }
 
 })(jQuery);
 
